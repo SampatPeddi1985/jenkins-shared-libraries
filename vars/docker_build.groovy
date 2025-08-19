e2270b3695895809e42dac0aa812f514c87596e1
SampatPeddi1985 <sampathkumar241085@gmail.com>
2025-08-19 12:06:42 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
 def class(String ProjectName, String ImageTag, String DockerHubUser{
- sh "docker build -t ${DockerHubUser}/${ProjectNmae}:${ImageTag}
+ sh "docker build -t ${DockerHubUser}/${ProjectNmae}:${ImageTag} ."
 }
